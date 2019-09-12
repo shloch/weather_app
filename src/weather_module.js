321101dbf0ec5ca244b54e7dc6034bfee363f07b
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2019-09-12 11:50:33 +0100

    REMOVING parenthesis from STICKLER error proposition

--- src/weather_module.js
+++ src/weather_module.js
@@ -8,8 +8,8 @@ import {
 
 export default function getWeatherInfos(location, units = 'metric') {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=617ec88a7d93c5b6ace5a90f054e9838&units=${units}`, { mode: 'cors' })
-    .then((response) => response.json())
-    .then((response) => {
+    .then(response => response.json())
+    .then(response => {
       if (units === 'metric') {
         celciusDisplay(response.main.temp);
         changeWeatherImg(response.main.temp);
